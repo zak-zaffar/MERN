308271925b2bccfabd1835125048134c1c42e6a3
zak-zaffar <zakzaffar@gmail.com>
2022-11-24 21:58:39 +0000

    fixed

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/Post.js';
+
+const Post = () => {
+    return(
+        <>
+        <h1>POST</h1>
+        <Post/>
+        <Post/>
+        </>
+    );
+}
+
+export default Post;
